dc89c1c07975c28c335c023dc64e2f1bc9d084f2
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-07-31 03:44:29 -0500

    added download tag -> make social links in new tab

--- pages/Contact.js
+++ pages/Contact.js
@@ -11,7 +11,7 @@ const ContactButton = () => {
 
   return (
     <button
-      className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold"
+      className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold duration-500 hover:scale-110"
       onClick={ContactMe}
     >
       Contact
