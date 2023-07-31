cb34deee99b311f9f7aed2d19594e737c1aa5d90
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-07-31 03:45:40 -0500

    Added Link to index page -> button to myGear page

--- pages/donwloadButton.js
+++ pages/donwloadButton.js
@@ -7,7 +7,7 @@ const DownloadButton = () => {
 
   return (
     <button
-      className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold"
+      className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold duration-500 hover:scale-110"
       onClick={downloadFile}
     >
       Resume
