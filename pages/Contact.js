8ce2b775f44c4c7b9646610ca03224fff6ff1786
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-01 21:02:44 -0500

    Added react-reveal library to the project.

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Slide from 'react-reveal/Slide'
 const ContactButton = () => {
   const ContactMe = () => {
     const email = 'chiedozie.ehileme@gmail.com';
@@ -10,12 +10,14 @@ const ContactButton = () => {
   };
 
   return (
-    <button
-      className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold duration-500 hover:scale-110"
-      onClick={ContactMe}
-    >
-      Contact
-    </button>
+    <Slide top>
+      <button
+        className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold duration-500 hover:scale-110"
+        onClick={ContactMe}
+      >
+        Contact
+      </button>
+    </Slide>
   );
 };
 
