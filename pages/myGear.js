7df607266ab77c25b96a5b97310afbb2441637f6
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-01 21:02:35 -0500

    Added react-reveal library to the project.

--- pages/myGear.js
+++ pages/myGear.js
@@ -2,11 +2,16 @@ import Head from 'next/head';
 import logo from '../public/logo.jpg'
 import { useState, useEffect } from 'react';
 import ToolTip from './ToolTip';
-import { BsFillMoonFill } from 'react-icons/bs';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import Slide from 'react-reveal/Slide';
+
 
 
 export default function Home() {
-    const [darkMode, setDarkMode] = useState("./index.js")
+    // Retrieve the darkMode value from local storage (returns "true" or "false")
+    const [darkMode, setDarkMode] = useState();
+    
+
 return (
     <div className={darkMode ? "dark": ""}>
         <meta
@@ -15,31 +20,38 @@ return (
         />
 
         <Head>
-            <title>My Gear</title>
+            <title>Gear</title>
             <meta name="viewport" content="width=device-width, initial-scale=0.65"/>
             <link rel='icon' href='logo.jpg' />
         </Head>
 
 
         <main className='bg-white px-10 md:px-20 lg:px-14 transition-all duration-700 dark:bg-gray-900'>
-            <section className='h-screen' >
-                <nav className='py-10 mb-12 flex justify-between'>
-                    <ToolTip tooltip={'Chiedozie Ehileme'}>
-                        <h1 className="text-xl font-burtons "><a href="/">cheidozie ehileme</a>
-                        </h1>
-                    </ToolTip>
-                    <ul className="flex items-center">
-                        <li>
-                            <ToolTip tooltip={ darkMode ? "Toggle Light Mode": "Toggle Dark"} >
-                                <BsFillMoonFill  className="cursor-pointer text-xl dark:text-yellow-400" 
-                                onClick={() => setDarkMode(!darkMode)}/>
-                            </ToolTip>
-                        </li>
-                    </ul>
-                </nav>
-            </section>
-            <h1>Start Web</h1>
+            <Slide top>
+                <section className='h-screen' >
+                    <nav className='py-10 mb-12 flex justify-between'>
+                        <ToolTip tooltip={'Chiedozie Ehileme'}>
+                            <h1 className='font-burtons dark:text-white text-xl '>
+                                <a href='/'> 
+                                chiedozie ehileme
+                                </a>
+                            </h1>
+                            
+                        </ToolTip>
+                        <ul className="flex items-center">
+                            <li>
+                                <ToolTip tooltip={ darkMode ? "Toggle Light Mode": "Toggle Dark Mode"} >
+                                    <BsFillMoonStarsFill  className="cursor-pointer text-xl dark:text-yellow-400 mx-10" 
+                                    onClick={() => setDarkMode(!darkMode)}/>
+                                </ToolTip>
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+            
+            </Slide>
         </main>
+
     </div>
     )
 }
