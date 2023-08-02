d02f79130a1368bd1d04941a1246845eb50e138c
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-01 21:02:03 -0500

    Added react-reveal to the project.

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,30 @@ import { useRouter } from "next/router";
 import headshot from "../public/headshot.jpg"
 import ToolTip from "./ToolTip";
 import DownloadButton from "./donwloadButton";
+import { motion, AnimatePresence} from "framer-motion";
 import ContactButton from "./Contact";
+import React from "react";
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+import Flip from 'react-reveal/Flip'
 
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Load the darkMode state from Local Storage on initial render
+    const savedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (savedDarkMode !== null) {
+      setDarkMode(savedDarkMode);
+    }
+  }, []);
+  
+  // Save the darkMode state to Local Storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
   const router = useRouter();
   
   const handleButtonClick = () => {
@@ -36,12 +55,12 @@ export default function Home() {
   }
   
   return (
-      
-    <div className={darkMode ? "dark": ""}>
+    <div className={darkMode ? "dark" : ""}>
       <meta
         name="format-detection"
         content="telephone=no, date=no, email=no, address=no"
-    />
+      />
+    
     <time datetime="2016-10-25" suppressHydrationWarning />
     
       <Head>
@@ -49,59 +68,93 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=0.65"/>
         <link rel="icon" href="logo.jpg" />
       </Head>
-      <main className="bg-white px-10 md:px-20 lg:px-14 transition-all duration-700 dark:bg-gray-900 "> {/* add fade in to dark mode feature */}
-         <section className=" h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
-          <ToolTip tooltip={"Chiedozie Ehileme"} >
-              <h1 className="text-xl font-burtons dark:text-white " ><a href="localhost:3000" >chiedozie ehileme</a></h1> 
-          </ToolTip>
-              <ul className="flex items-center">
-                <li>
-                  <ToolTip tooltip={ darkMode ? "Toggle Light Mode": "Toggle Dark"}  >
-                    <BsFillMoonStarsFill  className="cursor-pointer text-xl dark:text-yellow-400" 
-                  onClick={() => setDarkMode(!darkMode)}
-                  
-                  />
-                  </ToolTip>
-                  
-                  
-                </li>
-                <li>
-                  <DownloadButton />
-                </li>
-                <li>
-                  <ContactButton  />
-                </li>
-              </ul>
-          </nav>
-          <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-blue-500 font-medium md:text-6xl">Chiedozie Ehileme</h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Computer Science Major.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">Current Computer Science Major with the goal of becoming a software engineer; Future CEO of Cephrius Technologies</p>
-          </div>
-          <section>
-            <div className="flex justify-center">
-              <button className="bg bg-gradient-to-r from-blue-500 to-blue-400 rounded-md px-12 py-2 font-bold text-white duration-200 hover:scale-125"
-              onClick={handleButtonClick}>
-                Check Out My Gear
-              
-              </button>
-            </div>
-          </section>
 
-          <div className="text-6xl flex justify-center gap-16 py-10 text-gray-600 dark:text-white ">
-            <ToolTip textSize="lg" tooltip={"Twitter"}><a href="https://twitter.com/CEhileme" target="_blank" rel="noreferrer"><AiFillTwitterCircle className="duration-300 hover:scale-125" /></a></ToolTip>
-            <ToolTip textSize="lg" tooltip={"Instagram"}><a href="https://www.instagram.com/chiedozie.py/" target="_blank" rel="noreferrer" ><AiFillInstagram className="duration-300 hover:scale-125"/> </a> </ToolTip>
-            <ToolTip textSize="lg" tooltip={"Linkedin"}  ><a href="https://www.linkedin.com/in/chiedozie-ehileme-529b6a25b/" target="_blank" rel="noreferrer"><AiFillLinkedin className="duration-300 hover:scale-125" /></a></ToolTip>
-            <ToolTip textSize="lg" tooltip={"GitHub"}  ><a href="https://github.com/Cephrius/" target="_blank" rel="noreferrer"><AiFillGithub className="duration-300 hover:scale-125" /></a></ToolTip>
-            
-            
-            
-          </div>
-          <div className="relative mx-auto bg-gradient-to-b from-blue-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-            <Image src={headshot} objectFit="cover"/>
-          </div>
+      
+      <main className="bg-white px-10 md:px-20 lg:px-14 transition-all duration-700 dark:bg-gray-900 "> {/* add fade in to dark mode feature */}
+        
+          <section className=" h-screen">
+            <Slide top >
+              <nav className="py-10 mb-12 flex justify-between">
+                <ToolTip tooltip={"Chiedozie Ehileme"} > 
+                  <Slide top>
+                    <h1 className="text-xl font-burtons dark:text-white " ><a href="localhost:3000" >chiedozie ehileme</a></h1>
+                  </Slide>
+                </ToolTip>
+                    <ul className="flex items-center">
+                      <li>
+                          <ToolTip tooltip={ darkMode ? "Toggle Light Mode": "Toggle Dark Mode"}  >
+                            <Slide top>
+                                <BsFillMoonStarsFill  className="cursor-pointer text-xl dark:text-yellow-400" 
+                                  onClick={() => setDarkMode(!darkMode)}
+                                />
+                            </Slide>
+                          </ToolTip>
+                          
+                      </li>
+                        <li>
+                          <DownloadButton />
+                        </li>
+                        <li>
+                          <ContactButton  />
+                        </li>
+                    </ul>
+              </nav>
+            </Slide>
+          <Slide top small >
+            <div className="text-center p-10 ">
+              <h2 className="text-5xl py-2 text-blue-500 font-medium md:text-6xl">Chiedozie Ehileme</h2>
+              <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Computer Science Major.</h3>
+              <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">Current Computer Science Major with the goal of becoming a software engineer; Future CEO of Cephrius Technologies</p>
+            </div>
+          </Slide>
+          <section>
+            <Flip top big cascade>
+              <div className="flex justify-center">
+                  <button className="bg bg-gradient-to-r from-blue-500 to-blue-400 rounded-md px-12 py-2 font-bold text-white duration-200 hover:scale-125"
+                    onClick={handleButtonClick}>
+                      Check Out My Gear
+                  </button>
+              </div>
+            </Flip>
+          </section>
+    
+          
+            <div className="text-6xl flex justify-center gap-16 py-10 text-gray-600 dark:text-white ">
+              
+                <ToolTip textSize="lg" tooltip={"Linkedin"}  ><a href="https://www.linkedin.com/in/chiedozie-ehileme-529b6a25b/" target="_blank" rel="noreferrer">
+                    <Slide left big>
+                      <AiFillLinkedin className="duration-300 hover:scale-125" />
+                    </Slide>
+                  </a>
+                </ToolTip>
+
+                <ToolTip textSize="lg" tooltip={"GitHub"}  ><a href="https://github.com/Cephrius/" target="_blank" rel="noreferrer">
+                  <Slide left big >
+                    <AiFillGithub className="duration-300 hover:scale-125" />
+                  </Slide>
+                  </a>
+                </ToolTip>
+                <ToolTip textSize="lg" tooltip={"Instagram"}><a href="https://www.instagram.com/chiedozie.py/" target="_blank" rel="noreferrer" >
+                  <Slide right big>
+                      <AiFillInstagram className="duration-300 hover:scale-125"/> 
+                  </Slide> 
+                  </a> 
+                </ToolTip>
+                <ToolTip textSize="lg" tooltip={"Twitter"}><a href="https://twitter.com/CEhileme" target="_blank" rel="noreferrer">
+                  <Slide right big>
+                    <AiFillTwitterCircle className="duration-300 hover:scale-125" />
+                  </Slide>
+                  </a>
+                </ToolTip>
+             
+            </div>
+          <Slide top >
+            <div className="relative mx-auto bg-gradient-to-b from-blue-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+              <Image src={headshot} objectFit="cover"/>
+            </div>
+          </Slide>
          </section>
+         
          <section>
           <div className="">
             <h2 className="text-4xl py-2 text-center dark:text-white">About Me</h2>
@@ -161,7 +214,7 @@ export default function Home() {
             
             <h3 className="text-4xl py-1 text-center">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-center ">
-              Here are some of the projects that Ive worked on.
+              Here are some of the projects that I've worked on.
             </p>
           </div>
         <div className=" container mx-auto w-screen pb-28">
@@ -212,14 +265,14 @@ export default function Home() {
                 height={'100%'} 
                 layout="responsive" 
                 />
+              </div>
             </div>
-
           </div>
-        </div>
-          </section>
-         </main>
- 
+        </section>
+      </main>       
     </div>
+  
+  
   );
 
 };
