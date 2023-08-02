e3827f52559d2269abdc8e9463cb0df3d45abe53
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-02 02:18:26 -0500

    build(deps): update package-lock.json, package.json, Contact.js and donwloadButton.js

--- pages/donwloadButton.js
+++ pages/donwloadButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Slide from 'react-reveal';
+import { Slide,Bounce,Flip } from "react-awesome-reveal";
 
 
 const DownloadButton = () => {
@@ -8,7 +8,7 @@ const DownloadButton = () => {
   };
 
   return (
-    <Slide top>
+    <Slide direction="down" triggerOnce onVisibilityChange={false}>
     <button
       className="bg bg-gradient-to-r from-blue-500 to-blue-400 bg-blue-500 text-white px-10 py-2 rounded-lg ml-8 font-bold duration-500 hover:scale-110"
       onClick={downloadFile}
