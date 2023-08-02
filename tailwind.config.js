1fb41cd6c13ffae57d83f22b9c1e687a2ad151bb
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-08-02 16:36:13 -0500

    added slow bounce animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,29 @@ export const content = [
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
   // Or if using `src` directory:
   "./src/**/*.{js,ts,jsx,tsx,mdx}",
 ];
-
 
 export const theme = {
   extend: {
     fontFamily: {
       burtons: "burtons"
-    }
+    },
+    animation: {
+      'bounce-slow': 'bounceSlow 2s cubic-bezier(0.2, 0.55, 0.25, 1.5) infinite',
+    },
+    keyframes: {
+      bounceSlow: {
+        '0%, 100%': {
+          transform: 'translateY(0)',
+        },
+        '50%': {
+          transform: 'translateY(-25px)', // Adjust the bounce height
+        },
+      },
+    },
   },
 };
+
 export const plugins = [];
